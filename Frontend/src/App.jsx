e96b65152c98b74b937c6fc43b76bcbd163c6cbe
Pavan Kusunuri <pavantejakusunuri@gmail.com>
2024-01-07 22:52:11 +0530

    Added Basic Routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { Routes, Route } from "react-router-dom";
+import Landing from "./Components/LandingPage/Landing";
+import Login from "./Components/Authentication/Login";
+import Register from "./Components/Authentication/Register";
+import AdminDashboard from "./Components/Admin/Dashboard";
+import AddMovie from "./Components/Admin/Movies/AddMovie"
+import ShowMovie from "./Components/Admin/Movies/ShowMovie";
+import EditMovie from "./Components/Admin/Movies/EditMovie";
+import Movies from "./Components/Admin/Movies/Movies.jsx";
+
+
 import './App.css'
 
 const App = () => {
 
+ 
+
   return (
-    <p className="text-3xl font-bold underline">User Choice</p>  
+    <Routes>
+    <Route path="/" element={<Landing/>} />
+    <Route path="/login" element={<Login/>} />
+    <Route path="/register" element={<Register/>} />
+    <Route path="/admin" element={<AdminDashboard />}>
+       <Route path="/admin/movies" element={<Movies/>} />
+      </Route>
+
+  </Routes>
+    // <p className="text-3xl font-bold underline">User Choice</p>  
   )
 }
 
